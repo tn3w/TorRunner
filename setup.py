8aafb18ce3d4609883ac446fcbaf424787e0d858
tn3w <tn3wA8xxfuVMs2@proton.me>
2024-07-29 20:20:36 +0200

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,25 @@
 from setuptools import setup, find_packages
 
-with open('README.md', 'r', encoding = 'utf-8') as readable_file:
+with open('README.md', 'r', encoding='utf-8') as readable_file:
     long_description = readable_file.read().split('> [!NOTE]')[0]
 
 setup(
-    name = "tor_runner",
-    version = "1.0.0",
-    description = "TorRunner is designed to facilitate the deployment of Tor hidden services.",
-    long_description = long_description,
-    author = 'TN3W',
-    author_email = 'tn3wA8xxfuVMs2@proton.me',
-    url = 'https://github.com/tn3w/TorRunner',
-    packages = find_packages(),
-    install_requires = [],
-    entry_points = {
+    name="tor_runner",
+    version="1.0.0",
+    description="TorRunner is designed to facilitate the deployment of Tor hidden services.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # Specify the content type
+    author='TN3W',
+    author_email='tn3wA8xxfuVMs2@proton.me',
+    url='https://github.com/tn3w/TorRunner',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
         'console_scripts': [
             'tor_runner=tor_runner.tor_runner:main',
         ],
     },
-    classifiers = [
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
@@ -37,7 +38,7 @@ setup(
         "Topic :: System :: Networking",
         "Topic :: System :: Systems Administration :: Authentication/Directory",
     ],
-    license = 'GPL-3.0',
-    keywords = 'flask tor hidden-service security privacy',
-    python_requires = '>=3.6',
+    license='GPL-3.0',
+    keywords='flask tor hidden-service security privacy',
+    python_requires='>=3.6',
 )
