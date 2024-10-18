55efdfdb7cb78037bc29a3602868a7356ba27c4f
TN3W <tn3w@protonmail.com>
2024-10-18 21:36:52 +0200

    Version 1.4.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as readable_file:
 
 setup(
     name = "tor_runner",
-    version = "1.4",
+    version = "1.4.1",
     description = "TorRunner is designed to facilitate the deployment of Tor hidden services.",
     long_description = long_description,
     long_description_content_type = 'text/markdown',
